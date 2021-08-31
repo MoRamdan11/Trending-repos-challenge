155de26fe1fd90f828670913616d1e4fb1a3e583
MoRadman11 <moramdan481@gmail.com>
2021-09-01 01:39:26 +0200

    Hero implementation

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar/navbar";
+import Hero from "../components/Hero/hero";
+const Home = () => {
+  return (
+    <header>
+      <Navbar />
+      <Hero />
+    </header>
+  );
+};
+export default Home;
